ad29190f4bc677c0abba7793e553bac3a3322eaa
Max Pfeifer <blackwolf5614@gmail.com>
2019-11-14 15:40:35 -0800

    -Played around with emmisive materials

---
+++ Assets/Scripts/MoveThings.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveThings : MonoBehaviour
+{
+    public Transform targetPoint;
+
+
+    public float lerpSpeed = 0.25f;
+    public float startDelay = 3f;
+
+    private void Start()
+    {
+        InvokeRepeating("MoveDaThing", startDelay, 0.01f);
+    }
+
+
+    void MoveDaThing()
+    {
+        //transform.position = Vector3.Lerp(transform.position, targetPoint.position, lerpSpeed);
+
+        transform.position = Vector3.MoveTowards(transform.position, targetPoint.position, lerpSpeed);
+    }
+}
